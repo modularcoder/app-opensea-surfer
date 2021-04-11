2bc10e13ffc50147ff478625174a70258e48529b
Gevorg Harutyunyan <gevharut@gmail.com>
2021-04-11 05:15:24 +0400

    UI adjustments

--- src/_common/BaseCardBundle/BaseCardBundle.tsx
+++ src/_common/BaseCardBundle/BaseCardBundle.tsx
@@ -1,5 +1,7 @@
+import clsx from 'clsx'
 import React from 'react'
 import { OpenSeaAssetBundle } from 'opensea-js/lib/types'
+import styles from './BaseCardBundle.module.css'
 
 export interface BaseCardBundleProps {
   data: OpenSeaAssetBundle
@@ -11,32 +13,61 @@ export const BaseCardBundle: React.FC<BaseCardBundleProps> = ({ data }) => {
   const { imagePreviewUrl, imageUrlThumbnail, owner } = firstAsset
 
   return (
-    <div className="flex flex-col overflow-hidden bg-white text-left w-full h-full   rounded-lg shadow-lg mb-8">
+    <div
+      className={clsx(
+        styles.BaseCardBundle,
+        'flex flex-col bg-white text-left w-full h-full rounded-lg shadow-lg mb-8',
+      )}
+    >
       <div className="flex flex-1 p-4">
         <div
-          className="
-            flex-none w-48 h-32 bg-white shadow-md sha overflow-hidden p-4 rounded relative
-          "
+          className={clsx(
+            styles.Thumbnail,
+            'flex-none w-48 h-32 bg-white shadow-md sha overflow-hidden p-4 rounded relative',
+          )}
         >
           <img
             src={imagePreviewUrl}
             alt=""
-            className="absolute inset-0 w-full h-full object-cover rounded"
+            className={clsx(
+              styles.ThumbnailImg,
+              'absolute inset-0 w-full h-full object-cover rounded',
+            )}
           />
         </div>
         <div className="flex-auto p-6">
           <div className="flex flex-wrap">
-            <h3 className="flex-auto text-lg text-indigo-600 font-semibold">{name}</h3>
+            <h3
+              className={clsx(
+                styles.Name,
+                'mb-4 h-14 flex-auto text-lg text-indigo-600 font-semibold',
+              )}
+            >
+              {name}
+            </h3>
             {/* <div class="text-xl font-semibold text-gray-500">$110.00</div>
             <div class="w-full flex-none text-sm font-medium text-gray-500 mt-2">
               In stock
             </div> */}
           </div>
-          <p className="text-sm text-gray-500">{description}</p>
+          <p
+            className={clsx(
+              styles.Description,
+              'text-sm text-gray-500 break-all overflow-ellipsis',
+            )}
+          >
+            {description}
+          </p>
         </div>
       </div>
-      <div className="flex flex-grow-0 p-4 overflow-hidden bg-gray-600">
-        <span className="text-gray-400 text-xs">Owner: {owner.address}</span>
+      <div
+        className="
+          flex flex-grow-0 p-4
+          rounded-b-lg
+          bg-gradient-to-br from-blue-400 to-indigo-400
+        "
+      >
+        <span className="text-white text-opacity-70 text-xs">Owner: {owner.address}</span>
       </div>
     </div>
   )
